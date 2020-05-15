e8adfe50344286936849a7604e1cf83a11677ee8
garedb <garedbarney@gmail.com>
2020-05-15 11:28:40 -0700

    New

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Require needed packages
 require('dotenv').config()
+let cors = require('cors')
 let express = require('express')
+let expressJwt = require('express-jwt')
 let morgan = require('morgan')
 let rowdyLogger = require('rowdy-logger')
 
@@ -12,9 +14,11 @@ let rowdyResults = rowdyLogger.begin(app)
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false })) // Accept form data
 app.use(express.json()) // Accept data from fetch (or any AJAX call)
+app.use(cors()) // TODO: Add react app as origin for cors
 
 // Routes
 app.use('/auth', require('./controllers/auth'))
+app.use('/profile', expressJwt({ secret: process.env.JWT_SECRET }), require('./controllers/profile'))
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' })
